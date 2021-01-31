9fa9337b8bf42c603848898599f31219b4b52d2d
Maaz <khanmaazm@gmail.com>
2021-01-31 17:40:27 +0400

    text

---
+++ app.py
@@ -0,0 +1,16 @@
+import flask
+app = flask.Flask(__name__)
+@app.route('/')
+def home():
+    return "<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p1></body></html>"
+
+@app.route('/aws', methods = ['GET'])
+def awshomepage():
+    return "Second Function Return - This is AWS Home PAge"
+
+@app.route('/azure', methods = ['GET'])
+def azurehomepage():
+
+    return "Second Function Return - This is Azure Home PAge"
+
+app.run()
--- application.py
+++ application.py
@@ -1,12 +1,10 @@
 import flask
-
 app = flask.Flask(__name__)
-@app.route('/', methods = ['GET'])
-def home():    
-   return "Default Home Function Return"
-
+@app.route('/')
+def home():
+    return "Default Home Function Return"
 @app.route('/second', methods = ['GET'])
-def home():    
-   return "Second Function Return"
+def home2():
+    return "Second Function Return"
 
 app.run()
